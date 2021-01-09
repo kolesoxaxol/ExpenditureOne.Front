d54fcb8246fc12125d8d181457aea0c94bee6c50
yevheniikoliesnik <yevhenii.ko@ideo-digital.com>
2021-01-09 11:35:23 +0200

    add test first integration with backend

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <CategoryForm />
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CategoryForm from './components/category/CategoryForm.vue'
+import Chart from './components/TestChart.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CategoryForm,
+    Chart
   }
 }
 </script>
---
+++ src/components/category/CategoryForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <h2>HTML Forms</h2>
+
+  <form>
+    <label for="fname">Category name:</label><br />
+    <input type="text" id="fname" name="fname" value="John" /><br />
+    <label for="lname">Category description</label><br />
+    <input type="text" id="lname" name="lname" value="Doe" /><br /><br />
+    <input type="button" value="Submit" @click="formSubmitHandler" />
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "CategoryForm",
+  props: {},
+  methods: {
+    formSubmitHandler: function () {
+  
+      const data = {
+        categoryName: "test",
+        color: "black",
+      };
+
+      axios
+        .post(`https://localhost:44352/api/category`, data, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then((response) => {
+          console.log("category added");
+        })
+        .catch((error) => {
+          console.log( error);
+        })
+        .finally(() => {});
+    },
+    data() {
+      return {};
+    },
+  },
+};
+</script>
